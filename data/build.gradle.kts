4db2875845d29bc8d5f727da6cee2690ab7b8a16
david <davidpozorojas@gmail.com>
2025-01-25 19:30:10 +0100

    feat: implement plugins in data and domain module

--- data/build.gradle.kts
+++ data/build.gradle.kts
@@ -2,23 +2,16 @@ import java.io.FileInputStream
 import java.util.Properties
 
 plugins {
-    alias(libs.plugins.android.library)
-    alias(libs.plugins.jetbrains.kotlin.android)
-    alias(libs.plugins.android.hilt)
-    kotlin("kapt")
-    alias(libs.plugins.kotlin.serialization)
+    id("sponsular.android.library")
+    id("sponsular.android.hilt")
+    id("sponsular.android.retrofit")
+    id("sponsular.android.serialization")
 }
 
 android {
     namespace = "com.sngular.data"
-    compileSdk = 34
 
     defaultConfig {
-        minSdk = 24
-
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-        consumerProguardFiles("consumer-rules.pro")
-
         val localProperties: Properties =
             Properties().apply {
                 load(FileInputStream(File(rootProject.rootDir, "local.properties")))
@@ -35,50 +28,8 @@ android {
             localProperties.getProperty("SPOTIFY_BASE_URL_TOKEN") ?: "",
         )
     }
-
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro",
-            )
-        }
-    }
-
-    buildFeatures {
-        buildConfig = true
-    }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
-    }
-    kotlinOptions {
-        jvmTarget = JavaVersion.VERSION_17.toString()
-    }
 }
 
 dependencies {
-
     implementation(project(":domain"))
-
-    implementation(libs.androidx.core.ktx)
-
-    //Network
-    implementation(libs.retrofit)
-    implementation(libs.retrofit.converter.json)
-    implementation(libs.okhttp3)
-    implementation(libs.okhttp3.interceptor.logging)
-
-    //Hilt
-    implementation(libs.hilt)
-    kapt(libs.hilt.compiler)
-
-    //Json serializer
-    implementation(libs.serialization.json)
-
-    //testing Pruebas
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
 }
--- domain/build.gradle.kts
+++ domain/build.gradle.kts
@@ -1,45 +1,8 @@
 plugins {
-    alias(libs.plugins.android.library)
-    alias(libs.plugins.jetbrains.kotlin.android)
-    alias(libs.plugins.kotlin.serialization)
+    id("sponsular.android.library")
+    id("sponsular.android.serialization")
 }
 
 android {
     namespace = "com.sngular.domain"
-    compileSdk = 34
-
-    defaultConfig {
-        minSdk = 24
-
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-        consumerProguardFiles("consumer-rules.pro")
-    }
-
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro",
-            )
-        }
-    }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
-    }
-    kotlinOptions {
-        jvmTarget = JavaVersion.VERSION_17.toString()
-    }
-}
-
-dependencies {
-
-    implementation(libs.androidx.core.ktx)
-
-    implementation(libs.serialization.json)
-
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
 }

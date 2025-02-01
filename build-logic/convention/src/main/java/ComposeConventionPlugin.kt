01b04af5ece91e95069b4fec4d6a884cdcfb094c
dprdeveloper <davidpozorojas@gmail.com>
2025-02-01 14:08:12 +0100

    feat: Implement navigation, added compose compiler plugin and update kotlin version

--- build-logic/convention/src/main/java/ComposeConventionPlugin.kt
+++ build-logic/convention/src/main/java/ComposeConventionPlugin.kt
@@ -2,19 +2,15 @@ import com.android.build.api.dsl.LibraryExtension
 import com.sngular.sponsular.libs
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import org.gradle.kotlin.dsl.apply
 import org.gradle.kotlin.dsl.configure
 import org.gradle.kotlin.dsl.dependencies
 
 class ComposeConventionPlugin: Plugin<Project> {
     override fun apply(target: Project) {
         with(target) {
-            extensions.configure<LibraryExtension> {
-                buildFeatures {
-                    compose = true
-                }
-                composeOptions {
-                    kotlinCompilerExtensionVersion = libs.findVersion("kotlinCompilerExtension").get().toString()
-                }
+            with(pluginManager){
+                apply("org.jetbrains.kotlin.plugin.compose")
             }
 
             dependencies {
@@ -25,6 +21,7 @@ class ComposeConventionPlugin: Plugin<Project> {
                 add("implementation",libs.findLibrary("androidx-ui-graphics").get())
                 add("implementation",libs.findLibrary("androidx-ui-tooling-preview").get())
                 add("implementation", libs.findLibrary("androidx.material3").get())
+                add("implementation", libs.findLibrary("androidx-navigation-compose").get())
 
                 add("androidTestImplementation",platform(libs.findLibrary("androidx-compose-bom").get()))
                 add("androidTestImplementation",libs.findLibrary("androidx-ui-test-junit4").get())
